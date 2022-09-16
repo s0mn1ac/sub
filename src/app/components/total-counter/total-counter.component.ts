89bd4f110923586d5baa294587b703a2dc38e3ac
Juan Miguel González Machado <juanmi.gonzalezm@gmail.com>
2022-09-16 20:05:19 +0100

    feat: dual or single button visibility by screen width

--- src/app/components/total-counter/total-counter.component.ts
+++ src/app/components/total-counter/total-counter.component.ts
@@ -1,5 +1,5 @@
 /* Angular */
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, HostListener, Input, OnDestroy, OnInit } from '@angular/core';
 
 /* RxJs */
 import { Observable, Subject, takeUntil } from 'rxjs';
@@ -23,7 +23,7 @@ import { PlanTypeEnum } from 'src/app/shared/enums/plan-type.enum';
   templateUrl: './total-counter.component.html',
   styleUrls: ['./total-counter.component.scss'],
 })
-export class TotalCounterComponent implements OnInit, OnDestroy {
+export class TotalCounterComponent implements OnInit, OnDestroy, AfterViewInit {
 
   @Input() totalPerMonth: number;
   @Input() totalPerYear: number;
@@ -37,6 +37,11 @@ export class TotalCounterComponent implements OnInit, OnDestroy {
 
   private totalPeriod: PlanTypeEnum;
 
+  @HostListener('window:resize')
+  onResize() {
+    this.updateNavBarVisibility();
+  }
+
   constructor(
     private storageService: StorageService,
     private store: Store<AppState>
@@ -45,6 +50,10 @@ export class TotalCounterComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.initStoreSelectors();
     this.initStoreSubscriptions();
+  }
+
+  ngAfterViewInit(): void {
+    this.updateNavBarVisibility();
   }
 
   ngOnDestroy(): void {
@@ -67,6 +76,18 @@ export class TotalCounterComponent implements OnInit, OnDestroy {
     this.totalPeriod$
       .pipe(takeUntil(this.destroy$))
       .subscribe((totalPeriod: PlanTypeEnum) => this.totalPeriod = totalPeriod);
+  }
+
+  private updateNavBarVisibility(): void {
+    const buttonsContainer: HTMLElement | null = document.getElementById('navBarButtonsContainer');
+    if (buttonsContainer === null) {
+      return
+    }
+    if (buttonsContainer.offsetWidth <= 130 && !buttonsContainer.classList.contains('fab-group')) {
+      buttonsContainer.classList.add('fab-group');
+    } else if (buttonsContainer.offsetWidth > 130 && buttonsContainer.classList.contains('fab-group')) {
+      buttonsContainer.classList.remove('fab-group');
+    }
   }
 
 }
